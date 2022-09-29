8dbaaa80639ababb95084f0afc4ebe249e4a2e5c
Ubuntu <azureuser@frontend-vm-01.cjd0ubzykv5e3f4uvue1zk5bwe.ax.internal.cloudapp.net>
2022-09-29 04:58:38 +0000

    merege fixes on views.py

--- app/views.py
+++ app/views.py
@@ -1,11 +1,19 @@
 import os
+import json
+from os import environ as env
 from sys import set_coroutine_origin_tracking_depth
 from flask import Flask, request, jsonify , render_template
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import select , and_
 from app import app
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://startyounguk:DBpassword01@startyoungukdb.postgres.database.azure.com/postgres'
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://startyounguk:DBpassword01@startyoungukdb.postgres.database.azure.com/postgres'
+#app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+app.config["SQLALCHEMY_DATABASE_URI"] = env["SQLALCHEMY_DATABASE_URI"]
+# String (Mapped Name)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = env["SQLALCHEMY_TRACK_MODIFICATIONS"]
+
 
 db = SQLAlchemy(app)
 
@@ -33,16 +41,45 @@ class Student(db.Model):
   st_age=db.Column(db.Integer)
   st_location=db.Column(db.String(40))
   st_contact=db.Column(db.String(40))
+  st_hby=db.Column(db.String(10))
   st_school=db.Column(db.String(40))
+  st_spon_status=db.Column(db.String(5))
+  st_is_appr=db.Column(db.String(5))
 
 
-  def __init__(self,fname,lname,st_age,st_location,st_contact,st_school):
+  def __init__(self,fname,lname,st_age,st_location,st_contact,st_hby,st_school):
     self.fname=fname
     self.lname=lname
     self.st_age=st_age
     self.st_location=st_location
     self.st_contact=st_contact
+    self.st_hby=st_hby
     self.st_school=st_school
+
+class Sponsor(db.Model):
+  __tablename__='sponsor'
+  id=db.Column(db.Integer,primary_key=True)
+  fname=db.Column(db.String(20))
+  lname=db.Column(db.String(20))
+  sp_age=db.Column(db.Integer)
+  sp_location=db.Column(db.String(40))
+  sp_contact=db.Column(db.String(40))
+  sp_email=db.Column(db.String(40))
+  sp_hby=db.Column(db.String(10))
+  sp_occ=db.Column(db.String(40))
+  sp_is_appr=db.Column(db.String(5))
+
+
+  def __init__(self,fname,lname,sp_age,sp_location,sp_contact,sp_email,sp_hby,sp_occ):
+    self.fname=fname
+    self.lname=lname
+    self.sp_age=sp_age
+    self.sp_location=sp_location
+    self.sp_contact=sp_contact
+    self.sp_hby=sp_hby
+    self.sp_occ=sp_occ
+    self.sp_email=sp_email
+
 
 
 @app.route("/")
@@ -78,18 +115,90 @@ def addStudent():
 def addSchool():
     return render_template("addSchool2.html")
 
+@app.route("/fetchDB")
+def fetchDBPulic():
+    sc_all = School.query.all()
+    st_all = Student.query.all()
+    sp_all = Sponsor.query.all()
+
+    st_results = [
+            {
+                'name': student.fname,
+                'lastname': student.lname,
+                'st_school': student.st_school,
+                'st_spon_status': student.st_spon_status
+            } for student in st_all ]
+    sc_results = [
+            {
+                'sc_id': school.sc_id,
+                'sc_name': school.sc_name
+            } for school in sc_all ]
+    sp_results = [
+            {
+                'name': sponsor.fname,
+                'lastname': sponsor.lname
+            } for sponsor in sp_all ]
 
 
-@app.route("/submitStudent", methods=['POST'])
+    st_pending_scrng_stmt = select([Student.fname,Student.lname]).where(and_( Student.st_is_appr==None))
+    st_pending_scrng = [dict(row) for row in db.session.execute(st_pending_scrng_stmt)]
+
+    st_pending_spnsr_stmt = select([Student.fname,Student.lname]).where(and_( Student.st_is_appr=='yes', Student.st_spon_status==None))
+    st_pending_spnsr = [dict(row) for row in db.session.execute(st_pending_spnsr_stmt)]
+
+    return { "School's registered": len(sc_results) , "Total Students": len(st_results) , "Students pending sponsorship": len(st_pending_spnsr) , "Students under screening and onboarding": len(st_pending_scrng), "Our Sponsors": len(sp_results) }
+
+
+@app.route("/fetchDBPriv")
+def fetchDBPrivate():
+    sc_all = School.query.all()
+    st_all = Student.query.all()
+    sp_all = Sponsor.query.all()
+
+    st_results = [
+            {
+                'name': student.fname,
+                'lastname': student.lname,
+                'st_school': student.st_school,
+                'st_spon_status': student.st_spon_status
+            } for student in st_all ]
+    sc_results = [
+            {
+                'sc_id': school.sc_id,
+                'sc_name': school.sc_name
+            } for school in sc_all ]
+    sp_results = [
+            {
+                'name': sponsor.fname,
+                'lastname': sponsor.lname
+            } for sponsor in sp_all ]
+
+
+    sp_pending_scrng_stmt = select([Sponsor.fname,Sponsor.lname]).where(and_( Sponsor.sp_is_appr==None))
+    sp_pending_scrng = [dict(row) for row in db.session.execute(sp_pending_scrng_stmt)]
+
+    st_pending_scrng_stmt = select([Student.fname,Student.lname]).where(and_( Student.st_is_appr==None))
+    st_pending_scrng = [dict(row) for row in db.session.execute(st_pending_scrng_stmt)]
+
+    st_pending_spnsr_stmt = select([Student.fname,Student.lname]).where(and_( Student.st_is_appr=='yes', Student.st_spon_status==None))
+    st_pending_spnsr = [dict(row) for row in db.session.execute(st_pending_spnsr_stmt)]
+
+
+    return (render_template('adminDashboard.html', Stdata=st_results , Scdata=sc_results , StPnSp=st_pending_scrng , SpPnSp=sp_pending_scrng))
+
+
+
+@app.route("/submit", methods=['POST'])
 def submit():
   fname= request.form['fname']
   lname=request.form['lname']
   st_age=request.form['st_age']
   st_location=request.form['st_location']
   st_contact=request.form['st_contact']
+  st_hby=request.form['hobby']
   st_school=request.form['st_school']
 
-  student=Student(fname,lname,st_age,st_location,st_contact,st_school)
+  student=Student(fname,lname,st_age,st_location,st_contact,st_hby,st_school)
   db.session.add(student)
   db.session.commit()
 
@@ -99,7 +208,6 @@ def submit():
     print(result.fname)
 
   return render_template('success.html', data=fname)
-
 
 
 @app.route("/submitSchool", methods=['POST'])
